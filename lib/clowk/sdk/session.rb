2cb8f3837990d1be3fb8f21fe257a59d271fc197
thadeu <tadeuu@gmail.com>
2026-03-23 14:41:33 -0300

    Add search and revoke methods to Session class with corresponding tests

--- lib/clowk/sdk/session.rb
+++ lib/clowk/sdk/session.rb
@@ -6,6 +6,19 @@ module Clowk
       def self.resource_path
         'sessions'
       end
+
+      # @param email [String] Email to search for (ILIKE match)
+      # @return [Clowk::Http::Response]
+      def search(email:)
+        client.get("#{self.class.resource_path}/search?email=#{ERB::Util.url_encode(email)}")
+      end
+
+      # Revokes a session by its session_id (clk_session_UUID)
+      # @param session_id [String]
+      # @return [Clowk::Http::Response]
+      def revoke(session_id)
+        destroy(session_id)
+      end
     end
   end
 end
